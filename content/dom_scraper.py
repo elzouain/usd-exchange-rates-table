8a94b4fe6287f26c963d7bfdd318f199938d7891
Simon Zouain <elzouain@gmail.com>
2023-01-24 13:37:40 -0500

    User can scrape different regions. - [Refactor] ConfigReader class was refactored into a singleton. - [Update] Region '-r' and rows '-m' flag functionality was implemented.

--- content/dom_scraper.py
+++ content/dom_scraper.py
@@ -1,18 +1,20 @@
-from utils.page_parser import prepare_soup
+from utils.config_reader import *
 import os
 
 
-def find_selected_region():
+def read_selected_region_tab():
     """
     Locates the selected region tab and sets the value 'region' in the system variables.
     :return: A string of the region.
     """
-    os.environ['region'] = prepare_soup().find('div', {'class': 'tabs'}).find('a', {'class': 'active'}).text
+    c = ConfigReader.get_instance()
+    os.environ['region'] = c.get_soup().find('div', {'class': 'tabs'}).find('a', {'class': 'active'}).text
     return os.environ.get('region')
 
 
 def extract_rates_data():
-    table = prepare_soup().find('div', {'class': 'table-responsive'}).find('table')
+    c = ConfigReader.get_instance()
+    table = c.get_soup().find('div', {'class': 'table-responsive'}).find('table')
     table_content = []
     for tr in table.find('tbody').find_all('tr'):
         row_data = ["", "", "", ""]
@@ -30,4 +32,6 @@ def extract_rates_data():
                 # i.e 56.10
                 row_data[3] = td[i].text.strip().replace(" ", "")
         table_content.append(row_data)
+        if len(table_content) == c.get_max_rows():
+            break
     return table_content
